6ed68c1a1a85062224cd36c18dace2560f4b707a
Gaetan <gaetan.marchale@hotmail.com>
2022-12-07 15:55:16 +0100

    Day Four

--- 2022/Day04/Part_Two.py
+++ 2022/Day04/Part_Two.py
@@ -8,10 +8,6 @@ for i in line:
     liste1 = [x for x in range(int(liste[0]), int(liste[1]) + 1)]
     liste2 = [x for x in range(int(liste[2]), int(liste[3]) + 1)]
     stop = 0
-    #for x in range(int(liste[0]), int(liste[1]) + 1) :
-     #   liste1.append(x)
-    #for x in range(int(liste[2]), int(liste[3]) + 1) :
-     #   liste2.append(x)
     for y in liste1 :
         if y in liste2 and stop == 0:
             total += 1
